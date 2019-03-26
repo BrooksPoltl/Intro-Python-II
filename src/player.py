e841b81d2b2fbbb2e49452524d7c54b31170d3bc
brooks <bpoltl1@gmail.com>
2019-03-26 14:44:33 -0500

    added classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Room:
+    def __init__(self, information, currentRoom):
+        self.information = information
+        self.currentRoom = currentRoom
+    def __str__(self):
+        return str(self.__dict__)
